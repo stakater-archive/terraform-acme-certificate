9a9cbb7c4519dd428f581632ca10e99e5842ef01
Muhammad Hamza Zaib <hamza.zaib@curity.io>
2020-05-18 15:48:32 +0200

    Add intermediate cert to combined secret

--- scripts/generate-combined-secret.sh
+++ scripts/generate-combined-secret.sh
@@ -1,6 +1,7 @@
 cat <<-EOF > cert-secret.yaml
 apiVersion: v1
 data:
+  ca.crt: "$(terraform output -module=cert-stakater public_certificate_intermediate_pem | base64 --wrap=0)"
   tls.crt: "$(terraform output -module=cert-stakater complete_public_certificate_key | base64 --wrap=0)"
   tls.key: "$(terraform output -module=cert-stakater public_certificate_key | base64 --wrap=0)"
 kind: Secret
